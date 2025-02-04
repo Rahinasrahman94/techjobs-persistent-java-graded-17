d0f6ae58281c3c07359f007179f149ca76eef7a4
rahinasrahman <rshinasrahman94@gmail.com>
2025-02-03 19:07:42 -0600

    GA 4 WITH ALL SQL

--- queries.sql
+++ queries.sql
@@ -5,7 +5,7 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
 DROP TABLE job;
 --Part 4
-SELECT name ,description  FROM skill WHERE skill.id in(SELECT job_skills.skills_id FROM  job_skills
-INNER JOIN  job ON job_skills.jobs_id = job.id WHERE job_skills.job_id IS NOT NULL) ORDER BY name ASC;
+SELECT * FROM skill LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+                WHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
 
 
--- src/test/java/org/launchcode/techjobs/persistent/TestTaskTwo.java
+++ src/test/java/org/launchcode/techjobs/persistent/TestTaskTwo.java
@@ -549,12 +549,10 @@ public class TestTaskTwo extends AbstractTest {
     @Test
     public void testSqlQuery() throws IOException {
         String queryFileContents = getFileContents("queries.sql");
-        System.out.println(queryFileContents);
+
         Pattern queryPattern = Pattern.compile("SELECT\\s+name\\s+FROM\\s+employer\\s+WHERE\\s+location\\s+=\\s+\"St.\\s+Louis\\s+City\";", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
         Matcher queryMatcher = queryPattern.matcher(queryFileContents);
-        System.out.println(queryPattern.matcher(queryFileContents));
-        boolean queryFound = queryMatcher.find();
-        System.out.println(queryFound);
+        System.out.println(queryPattern.matcher(queryFileContents));  boolean queryFound = queryMatcher.find();
         assertTrue(queryFound, "Task 2 SQL query is incorrect. Test your query against your database to find the error.");
     }
 }
